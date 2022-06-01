7f4bd091ac4fafd7afbe73c5b30df72299f3ecbf
zzz0105 <97646647+zzz0105@users.noreply.github.com>
2022-06-01 14:24:50 +0900

    [Silver V] Title: 최대공약수와 최소공배수, Time: 108 ms, Memory: 113248 KB -BaekjoonHub

--- 백준/Silver/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 백준/Silver/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,13 +1,5 @@
-n, m = map(int, input().split())
-#math.gcd와 math.lcm을 사용하면 최대공약수 최소공배수 한방에 구할 수 있다
-
-#유클리드 호제법 이용해서 최대공약수 구하기
-x, y = n, m
-
-while True:
-    r = x % y
-    x, y = y, r
-    if r==0: break
-    
-print(x)            #최대공약수
-print(int(n*m/x))   #최소공배수
+x, y = map(int, input().split())
+z = x * y
+while y: x, y = y, x % y
+print(x)          
+print(int(z/x))   
