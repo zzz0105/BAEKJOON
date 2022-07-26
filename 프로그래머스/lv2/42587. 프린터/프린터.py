7061392f0dca7f28cf9fd67d3fdd1d2b601373ef
zzz0105 <97646647+zzz0105@users.noreply.github.com>
2022-07-27 02:08:00 +0900

    [level 2] Title: 프린터, Time: 0.05 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프린터/프린터.py
+++ 프로그래머스/lv2/42587. 프린터/프린터.py
@@ -1,13 +1,13 @@
 from collections import deque
 def solution(priorities, location):
-    d_priorities = deque(list(enumerate(priorities)))
+    d_priorities = deque(enumerate(priorities))
     cnt = 0
     while d_priorities:
         i, j = d_priorities.popleft()
-        if j==max(priorities):
+        if j == max(priorities):
             cnt += 1
             priorities.remove(j)
-            if i==location:
+            if i == location:
                 return cnt
         else:
             d_priorities.append((i, j))
