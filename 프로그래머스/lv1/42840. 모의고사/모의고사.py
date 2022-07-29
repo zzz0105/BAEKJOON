a957076b572e9ebb7b3132750087101f3cacd6ac
zzz0105 <97646647+zzz0105@users.noreply.github.com>
2022-07-29 23:43:24 +0900

    [level 1] Title: 모의고사, Time: 2.36 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -0,0 +1,25 @@
+def solution(answers):
+    answer = []
+    p1 = [1, 2, 3, 4, 5]
+    p2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    p3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    c1,c2,c3 = 0,0,0
+    
+    for i in range(len(answers)):
+        if answers[i] == p1[i%5]:
+            c1 += 1
+    for i in range(len(answers)):
+        if answers[i] == p2[i%8]:
+            c2 += 1
+    for i in range(len(answers)):
+        if answers[i] == p3[i%10]:
+            c3 += 1        
+            
+    c = [c1,c2,c3]
+    m = max(c)
+    
+    for i in range(len(c)):
+        if c[i]==m:
+            answer.append(i+1)
+            
+    return answer
