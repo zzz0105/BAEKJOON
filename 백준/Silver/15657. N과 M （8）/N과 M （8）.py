0c9e8fdc1d14699a42cab2f384b5edce94c7ab46
zzz0105 <97646647+zzz0105@users.noreply.github.com>
2022-08-30 13:04:05 +0900

    [Silver III] Title: N과 M (8), Time: 92 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -0,0 +1,15 @@
+def choose(ans):
+    if len(ans) == m:
+        print(*ans)
+        return 
+    else:
+        j = 0 if len(ans)==0 else nums.index(ans[-1])
+        for i in range(j, len(nums)):
+            ans.append(nums[i])
+            choose(ans)
+            ans.pop()
+
+n, m = map(int,input().split())
+nums = list(map(int,input().split()))
+nums.sort()
+choose([])
