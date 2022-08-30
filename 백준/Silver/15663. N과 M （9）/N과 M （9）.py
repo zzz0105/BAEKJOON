e9624191154cd5426b1d26e8344d16308690e21a
zzz0105 <97646647+zzz0105@users.noreply.github.com>
2022-08-31 00:48:12 +0900

    [Silver II] Title: N과 M (9), Time: 2368 ms, Memory: 120836 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -0,0 +1,22 @@
+from collections import Counter
+
+def choose(ans):
+    if len(ans) == m:
+        ans = ' '.join(map(str,ans))
+        if ans not in answer:
+            answer.append(ans)
+        return 
+    else:
+        for i in range(len(nums)):
+            if nums_cnt[nums[i]]>ans.count(nums[i]) :
+                ans.append(nums[i])
+                choose(ans)
+                ans.pop()
+
+n, m = map(int,input().split())
+nums = list(map(int,input().split()))
+nums.sort()
+nums_cnt = Counter(nums)
+answer = []
+choose([])
+print(*answer, sep='\n')
