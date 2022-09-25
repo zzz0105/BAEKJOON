21f914f57e32500ce27011a3840168c6adaf9966
zzz0105 <97646647+zzz0105@users.noreply.github.com>
2022-09-26 01:19:55 +0900

    [Silver II] Title: 랜선 자르기, Time: 468 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -3,7 +3,7 @@ wires = [int(input()) for _ in range(k)]
 l, r = 1, max(wires)
 while l <= r:
     m = (l + r) // 2
-    tmp = sum(map(lambda x:x//m, wires[:]))
+    tmp = sum(map(lambda x:x//m, wires))
     if n <= tmp:
         l = m + 1
     else:
