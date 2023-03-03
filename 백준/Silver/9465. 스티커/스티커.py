019dad2c5593cc550e73f09256076ed1b0812f31
zzz0105 <97646647+zzz0105@users.noreply.github.com>
2023-03-03 14:03:45 +0900

    [Silver I] Title: 스티커, Time: 1092 ms, Memory: 48076 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.py
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -10,7 +10,7 @@ for _ in range(T):
     if n > 1:
         for j in range(2):
             dp[j][1] = dp[j-1][0] + sticker[j][1]
-    for i in range(2,n):
-        for j in range(2):
-            dp[j][i] = max(dp[j-1][i-1], dp[j-1][i-2], dp[j][i-2]) + sticker[j][i]
+        for i in range(2,n):
+            for j in range(2):
+                dp[j][i] = max(dp[j-1][i-1], dp[j-1][i-2]) + sticker[j][i]
     print(max(dp[0][-1], dp[1][-1]))
