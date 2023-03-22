8b16f331149ff67fafe9b8c8f0768064382c5c4c
zzz0105 <97646647+zzz0105@users.noreply.github.com>
2023-03-22 15:45:28 +0900

    [Gold V] Title: ABCDE, Time: 1176 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.py
+++ 백준/Gold/13023. ABCDE/ABCDE.py
@@ -2,20 +2,25 @@ import sys
 input = sys.stdin.readline
 
 def check(arr):
-    global friends
+    global friends, visited
     if len(arr) == 5:
         print(1)
         exit(0)
     for friend in friends[arr[-1]]:
-        if friend not in arr:
+        if not visited[friend]:
+            visited[friend] = True
             check(arr+[friend])
+            visited[friend] = False
 
 N, M = map(int,input().split())
 friends = [[] for _ in range(N)]
+visited = [False]*N
 for _ in range(M):
     a, b = map(int,input().split())
     friends[a].append(b)
     friends[b].append(a)
 for i in range(N):
+    visited[i] = True
     check([i])
+    visited[i] = False
 print(0)
