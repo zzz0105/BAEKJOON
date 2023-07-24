b11345355cbe8eeee8e1183f07ce2dac137d1e8b
zzz0105 <97646647+zzz0105@users.noreply.github.com>
2023-07-24 17:37:58 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.05 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+def solution(X, Y):
+    x, y = Counter(X), Counter(Y)
+    answer = ''
+    for num in sorted(x.keys(), reverse=True):
+        if num in y.keys():
+            answer += num*min(x[num], y[num])
+    if answer: return "0" if answer[0]=="0" else answer
+    else:   return "-1"
+
