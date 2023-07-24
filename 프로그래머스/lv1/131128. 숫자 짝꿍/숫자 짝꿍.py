fff752595993019021bfe15e3fdc392b7c591a48
zzz0105 <97646647+zzz0105@users.noreply.github.com>
2023-07-24 17:38:42 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.06 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.py
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -1,11 +1,10 @@
 from collections import Counter
 
 def solution(X, Y):
-    x, y = Counter(X), Counter(Y)
     answer = ''
+    x, y = Counter(X), Counter(Y)
     for num in sorted(x.keys(), reverse=True):
         if num in y.keys():
             answer += num*min(x[num], y[num])
     if answer: return "0" if answer[0]=="0" else answer
     else:   return "-1"
-
