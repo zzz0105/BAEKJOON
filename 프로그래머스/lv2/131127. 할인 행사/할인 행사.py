25cad03cd133ed5799d7b69bdbda8792c63ea1b8
zzz0105 <97646647+zzz0105@users.noreply.github.com>
2023-07-26 00:35:06 +0900

    [level 2] Title: 할인 행사, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.py
@@ -1,21 +1,25 @@
 def solution(want, number, discount):
-    wd = {} # 원하는 것: 개수
-    dd = {} # 할인하는 물건: 구매 가능한 개수
+    wd = {}         # 원하는 것: 개수
+    dd = {}         # 할인하는 물건: 구매 가능한 개수
+    possible = {}   # 물건: 구매가능 여부
     for i in range(len(want)):
         wd[want[i]] = number[i]
         dd[want[i]] = 0
+        possible[want[i]] = False
         
-    days = 0 # 가능한 날
+    days = 0        # 가능한 날
     for i in range(len(discount)):
         if i >= 10:
-            dd[discount[i-10]] -= 1
-        dd[discount[i]] = dd.get(discount[i],0) + 1
-        
-        flag = 0
-        for item, ea in wd.items():
-            if dd[item] < ea:
-                break
-        else:
-            days += 1    
-
+            item = discount[i-10]
+            dd[item] -= 1
+            if item in wd.keys():
+                possible[item] = dd[item] >= wd[item]
+                
+        item = discount[i]
+        dd[item] = dd.get(item,0) + 1
+        if item in wd.keys():
+            possible[item] = dd[item] >= wd[item]  
+        if False not in possible.values():
+            days += 1
+            
     return days
